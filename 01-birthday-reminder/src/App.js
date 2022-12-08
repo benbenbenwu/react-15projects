ba8346a09b1baff17ecb3d8abefe73b5e29cf834
ben <ben@example.com>
2022-12-08 20:30:36 +0800

    final vision

--- 01-birthday-reminder/src/App.js
+++ 01-birthday-reminder/src/App.js
@@ -2,7 +2,20 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+
+  const [people, setPeople] = useState(data)
+
+  const clickFn = () => console.log('You clicked me');
+
+  return (
+    <main>
+      <section className='container'>
+        <h3>{people.length} birthdays today</h3>;
+        <List people={people} />
+        <button onClick={clickFn}>Click All</button>
+      </section>
+    </main>
+  )
 }
 
 export default App;
--- 01-birthday-reminder/src/List.js
+++ 01-birthday-reminder/src/List.js
@@ -1,11 +1,21 @@
 import React from 'react';
 
-const List = () => {
+const List = ({ people }) => {
+  const personArray = people.map(({ id, name, age, image }) => (
+    <article key={id} className='person'>
+      <img src={image} alt={name} />
+      <div>
+        <h4>{name}</h4>
+        <p>{age} years</p>
+      </div>
+    </article>
+  ))
   return (
     <>
-      <h2>list component</h2>
+      {personArray}
     </>
   );
-};
+}
+
 
 export default List;
